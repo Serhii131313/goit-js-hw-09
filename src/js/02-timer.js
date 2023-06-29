31342bfb5379dbccaa001572320791b099ca01b5
Serhii131313 <serhii.tarasov.skan@gmail.com>
2023-06-29 13:41:50 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -37,35 +37,32 @@ const flatpickrInstance = flatpickr('#datetime-picker', {
 function onBtnStartTimer() {
     refs.date.disabled = true;
     refs.btnStart.disabled = true;
-    timerId = setInterval(() => {
-        const currentTime = new Date(refs.date.value);
-        const deltaTime = currentTime - Date.now();
-        const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    timerId = setInterval(updateTimer, 1000);
+}
 
-        refs.days.textContent = addLeadingZero(days);
-        refs.hour.textContent = addLeadingZero(hours);
-        refs.minute.textContent = addLeadingZero(minutes);
-        refs.second.textContent = addLeadingZero(seconds);
+function updateTimer() {
+    const currentTime = new Date(refs.date.value);
+    const deltaTime = currentTime - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
 
-        if (deltaTime < 1000) {
-            clearInterval(timerId);
-            refs.date.disabled = false;
-        }
-    }, 1000);
+    refs.days.textContent = addLeadingZero(days);
+    refs.hour.textContent = addLeadingZero(hours);
+    refs.minute.textContent = addLeadingZero(minutes);
+    refs.second.textContent = addLeadingZero(seconds);
+
+    if (deltaTime < 1000) {
+        clearInterval(timerId);
+        refs.date.disabled = false;
+    }
 }
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+    const seconds = Math.floor(ms / 1000);
+    const minutes = Math.floor(seconds / 60) % 60;
+    const hours = Math.floor(seconds / 3600) % 24;
+    const days = Math.floor(seconds / 86400);
 
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds: seconds % 60 };
 }
 
 function addLeadingZero(value) {
@@ -75,3 +72,4 @@ function addLeadingZero(value) {
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
+
